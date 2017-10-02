5bb828f42d96ae1b3063a58cfe3869289b6fe93c
Nagrajan23 <c.raj.23@gmail.com>
2017-10-02 17:55:23 -0400

    Changes

--- MATLAB/jackalFirstRun.m
+++ MATLAB/jackalFirstRun.m
@@ -5,24 +5,25 @@
 % rosDevice = rosdevice('10.10.10.101','administrator','clearpath');
 % rosDevice = rosdevice('192.168.1.11','administrator','clearpath');
 % openShell(rosDevice)
-
 clear
 [velPub, velMsg] = rospublisher('cmd_vel');
+velMsg.Linear.X = -0.2;
 
-velMsg.Linear.X = 0.2;
-
-imuSub = rossubscriber('imu/data_raw');
-odomSub = rossubscriber('odometry/filtered');
-imuData = receive(imuSub,1);
-odomData = receive(odomSub,1);
-imuLog(1) = imuData;
-odomLog(1) = odomData;
+% imuSub = rossubscriber('imu/data_raw');
+% odomSub = rossubscriber('odometry/filtered');
+% imuData = receive(imuSub,1);
+% odomData = receive(odomSub,1);
+% imuLog(1) = imuData;
+% odomLog(1) = odomData;
+lidarSub = rossubscriber('/velodyne_points');
+lidarData(1) = receive(lidarSub,1);
 
 for i = 1:1000000
     send(velPub, velMsg);
-    imuData = receive(imuSub,1);
-    odomData = receive(odomSub,1);
-    imuLog(i) = imuData;
-    odomLog(i) = odomData;
+%     imuData = receive(imuSub,1);
+%     odomData = receive(odomSub,1);
+%     imuLog(i) = imuData;
+%     odomLog(i) = odomData;
+    lidarData(i) = receive(lidarSub,1);
 %     pause(1);
 end
